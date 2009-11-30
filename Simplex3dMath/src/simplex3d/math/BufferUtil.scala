1718cd00c87e49063c68287e8c6e181f4cf68e80
lexn82 <lexn82@fe83b338-bbbe-11de-ad49-1f49b0b801a7>
2009-11-30 20:25:25 +0000

    Changed licensing to LGPL v3+.

--- Simplex3dMath/src/simplex3d/math/BufferUtil.scala
+++ Simplex3dMath/src/simplex3d/math/BufferUtil.scala
@@ -1,48 +1,272 @@
 /*
- * Simplex3D, Math package
+ * Simplex3D, Math module
  * Copyright (C) 2009 Simplex3D team
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * This file is part of Simplex3d.
+ *
+ * Simplex3d is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Simplex3d is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * CLASSPATH EXCEPTION FOR UNMODIFIED WORK:
- * Linking this library statically or dynamically with other modules is making
- * a combined work based on this library. Thus, the terms and conditions of
- * the GNU General Public License cover the whole combination.
- *
- * As a special exception, the copyright holders of this library give you
- * permission to link this library with independent modules to produce
- * an executable, regardless of the license terms of these independent modules,
- * and to copy and distribute the resulting executable under terms of your
- * choice, provided that you also meet, for each linked independent module,
- * the terms and conditions of the license of that module. An independent module
- * is a module which is not derived from or based on this library. If you modify
- * this library in any way, then this exception is null and void and no longer
- * applies, in this case delete this exception statement from your version.
  */
 
 package simplex3d.math
 
-import java.nio.FloatBuffer
+import java.nio._
 
 
 /**
  * @author Aleksey Nikiforov (lex)
  */
-object BufferUtil {
+class BufferUtil {
 
-    // Matrix <-> buffer conversions
+    /**
+     * Column major order
+     */
+    def toArray(m: AnyMat2, a: Array[Float]) {
+        toArray(m, a, true, 0)
+    }
+    /**
+     * Column major order
+     */
+    def toBuffer(m: AnyMat2, buf: FloatBuffer) {
+        toBuffer(m, buf, true, 0)
+    }
+    /**
+     * Column major order
+     */
+    def toArray(m: AnyMat2, a: Array[Float], upcast: Boolean, offset: Int) {
+        import m._
+
+        if (upcast) {
+            a(offset) = m00
+            a(offset + 1) = m10
+            a(offset + 2) = 0
+            a(offset + 3) = 0
+            a(offset + 4) = m01
+            a(offset + 5) = m11
+            a(offset + 6) = 0
+            a(offset + 7) = 0
+            a(offset + 8) = 0
+            a(offset + 9) = 0
+            a(offset + 10) = 0
+            a(offset + 11) = 0
+            a(offset + 12) = 0
+            a(offset + 13) = 0
+            a(offset + 14) = 0
+            a(offset + 15) = 1
+        } else {
+            a(offset) = m00
+            a(offset + 1) = m10
+            a(offset + 3) = m01
+            a(offset + 4) = m11
+        }
+    }
+    /**
+     * Column major order
+     */
+    def toBuffer(m: AnyMat2, buf: FloatBuffer, upcast: Boolean, offset: Int) {
+        import m._
+
+        buf.position(offset)
+
+        if (upcast) {
+            buf.put(m00)
+            buf.put(m10)
+            buf.put(0)
+            buf.put(0)
+            buf.put(m01)
+            buf.put(m11)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(1)
+        } else {
+            buf.put(m00)
+            buf.put(m10)
+            buf.put(m01)
+            buf.put(m11)
+        }
+    }
+
+    /**
+     * Column major order
+     */
+    def toArray(m: AnyMat2x3, a: Array[Float]) {
+        toArray(m, a, true, 0)
+    }
+    /**
+     * Column major order
+     */
+    def toBuffer(m: AnyMat2x3, buf: FloatBuffer) {
+        toBuffer(m, buf, true, 0)
+    }
+    /**
+     * Column major order
+     */
+    def toArray(m: AnyMat2x3, a: Array[Float], upcast: Boolean, offset: Int) {
+        import m._
+
+        if (upcast) {
+            a(offset) = m00
+            a(offset + 1) = m10
+            a(offset + 2) = 0
+            a(offset + 3) = 0
+            a(offset + 4) = m01
+            a(offset + 5) = m11
+            a(offset + 6) = 0
+            a(offset + 7) = 0
+            a(offset + 8) = m02
+            a(offset + 9) = m12
+            a(offset + 10) = 0
+            a(offset + 11) = 0
+            a(offset + 12) = 0
+            a(offset + 13) = 0
+            a(offset + 14) = 0
+            a(offset + 15) = 1
+        } else {
+            a(offset) = m00
+            a(offset + 1) = m10
+            a(offset + 3) = m01
+            a(offset + 4) = m11
+            a(offset + 6) = m02
+            a(offset + 7) = m12
+        }
+    }
+    /**
+     * Column major order
+     */
+    def toBuffer(m: AnyMat2x3, buf: FloatBuffer, upcast: Boolean, offset: Int) {
+        import m._
+
+        buf.position(offset)
+
+        if (upcast) {
+            buf.put(m00)
+            buf.put(m10)
+            buf.put(0)
+            buf.put(0)
+            buf.put(m01)
+            buf.put(m11)
+            buf.put(0)
+            buf.put(0)
+            buf.put(m02)
+            buf.put(m12)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(1)
+        } else {
+            buf.put(m00)
+            buf.put(m10)
+            buf.put(m01)
+            buf.put(m11)
+            buf.put(m02)
+            buf.put(m12)
+        }
+    }
+
+    /**
+     * Column major order
+     */
+    def toArray(m: AnyMat3, a: Array[Float]) {
+        toArray(m, a, true, 0)
+    }
+    /**
+     * Column major order
+     */
+    def toBuffer(m: AnyMat3, buf: FloatBuffer) {
+        toBuffer(m, buf, true, 0)
+    }
+    /**
+     * Column major order
+     */
+    def toArray(m: AnyMat3, a: Array[Float], upcast: Boolean, offset: Int) {
+        import m._
+
+        if (upcast) {
+            a(offset) = m00
+            a(offset + 1) = m10
+            a(offset + 2) = m20
+            a(offset + 3) = 0
+            a(offset + 4) = m01
+            a(offset + 5) = m11
+            a(offset + 6) = m21
+            a(offset + 7) = 0
+            a(offset + 8) = m02
+            a(offset + 9) = m12
+            a(offset + 10) = m22
+            a(offset + 11) = 0
+            a(offset + 12) = 0
+            a(offset + 13) = 0
+            a(offset + 14) = 0
+            a(offset + 15) = 1
+        } else {
+            a(offset) = m00
+            a(offset + 1) = m10
+            a(offset + 2) = m20
+            a(offset + 3) = m01
+            a(offset + 4) = m11
+            a(offset + 5) = m21
+            a(offset + 6) = m02
+            a(offset + 7) = m12
+            a(offset + 8) = m22
+        }
+    }
+    /**
+     * Column major order
+     */
+    def toBuffer(m: AnyMat3, buf: FloatBuffer, upcast: Boolean, offset: Int) {
+        import m._
+
+        buf.position(offset)
+
+        if (upcast) {
+            buf.put(m00)
+            buf.put(m10)
+            buf.put(m20)
+            buf.put(0)
+            buf.put(m01)
+            buf.put(m11)
+            buf.put(m21)
+            buf.put(0)
+            buf.put(m02)
+            buf.put(m12)
+            buf.put(m22)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(0)
+            buf.put(1)
+        } else {
+            buf.put(m00)
+            buf.put(m10)
+            buf.put(m20)
+            buf.put(m01)
+            buf.put(m11)
+            buf.put(m21)
+            buf.put(m02)
+            buf.put(m12)
+            buf.put(m22)
+        }
+    }
+
     /**
      * Column major order
      */
@@ -176,7 +400,7 @@ object BufferUtil {
         import m._
 
         buf.position(offset)
-        
+
         buf.put(m00)
         buf.put(m10)
         buf.put(m20)
--- Simplex3dMath/src/simplex3d/math/RotationSubMat2.scala
+++ Simplex3dMath/src/simplex3d/math/RotationSubMat2.scala
@@ -1,35 +1,21 @@
 /*
- * Simplex3D, Math package
+ * Simplex3D, Math module
  * Copyright (C) 2009 Simplex3D team
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * This file is part of Simplex3d.
+ *
+ * Simplex3d is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Simplex3d is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * CLASSPATH EXCEPTION FOR UNMODIFIED WORK:
- * Linking this library statically or dynamically with other modules is making
- * a combined work based on this library. Thus, the terms and conditions of
- * the GNU General Public License cover the whole combination.
- *
- * As a special exception, the copyright holders of this library give you
- * permission to link this library with independent modules to produce
- * an executable, regardless of the license terms of these independent modules,
- * and to copy and distribute the resulting executable under terms of your
- * choice, provided that you also meet, for each linked independent module,
- * the terms and conditions of the license of that module. An independent module
- * is a module which is not derived from or based on this library. If you modify
- * this library in any way, then this exception is null and void and no longer
- * applies, in this case delete this exception statement from your version.
  */
 
 package simplex3d.math

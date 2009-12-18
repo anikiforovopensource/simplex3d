c2dfef3f5a2de7bea9004eb08dca471c1a918be7
lexn82 <lexn82@fe83b338-bbbe-11de-ad49-1f49b0b801a7>
2009-12-18 06:53:40 +0000

    Changed licensing headers to be more consistent.

--- Simplex3dMath/src/simplex3d/math/Vec3b.scala
+++ Simplex3dMath/src/simplex3d/math/Vec3b.scala
@@ -1,15 +1,15 @@
 /*
- * Simplex3D, Math module
- * Copyright (C) 2009 Simplex3D team
+ * Simplex3d, BaseMath module
+ * Copyright (C) 2009 Simplex3d Team
  *
- * This file is part of Simplex3d.
+ * This file is part of Simplex3dMath.
  *
- * Simplex3d is free software: you can redistribute it and/or modify
+ * Simplex3dMath is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Simplex3d is distributed in the hope that it will be useful,
+ * Simplex3dMath is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.

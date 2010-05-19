53d5f990550f7fc6714fe1fb59abf7b1f12bbd17
lexn82 <lexn82@fe83b338-bbbe-11de-ad49-1f49b0b801a7>
2010-05-19 00:26:52 +0000

    Moved matToBuffer() methods into math.package object.

--- Simplex3dMath/src/simplex3d/math/package.scala
+++ Simplex3dMath/src/simplex3d/math/package.scala
@@ -20,6 +20,8 @@
 
 package simplex3d
 
+import java.nio._
+
 
 /**
  * @author Aleksey Nikiforov (lex)
@@ -478,4 +480,105 @@ package object math {
    * @return a boolean vector with negated components of u.
    */
   def not(u: inVec4b) :Vec4b = Vec4b(!u.x, !u.y, !u.z, !u.w)
+
+  def matToArray(m: ReadMat[_], array: Array[Float]) { matToArray(m, array, 0) }
+
+  /** Column major order.
+   *
+   */
+  def matToArray(m: ReadMat[_], array: Array[Float], offset: Int) {
+    array(offset + 0) = m.f00
+    array(offset + 1) = m.f10
+    array(offset + 2) = m.f20
+    array(offset + 3) = m.f30
+
+    array(offset + 4) = m.f01
+    array(offset + 5) = m.f11
+    array(offset + 6) = m.f21
+    array(offset + 7) = m.f31
+
+    array(offset + 8) = m.f02
+    array(offset + 9) = m.f12
+    array(offset + 10)= m.f22
+    array(offset + 11)= m.f32
+
+    array(offset + 12)= m.f03
+    array(offset + 13)= m.f13
+    array(offset + 14)= m.f23
+    array(offset + 15)= m.f33
+  }
+
+  /** Use buffer.position to change offset.
+   *
+   */
+  def matToBuffer(m: ReadMat[_], buffer: FloatBuffer) {
+    buffer.put(m.f00)
+    buffer.put(m.f10)
+    buffer.put(m.f20)
+    buffer.put(m.f30)
+
+    buffer.put(m.f01)
+    buffer.put(m.f11)
+    buffer.put(m.f21)
+    buffer.put(m.f31)
+
+    buffer.put(m.f02)
+    buffer.put(m.f12)
+    buffer.put(m.f22)
+    buffer.put(m.f32)
+
+    buffer.put(m.f03)
+    buffer.put(m.f13)
+    buffer.put(m.f23)
+    buffer.put(m.f33)
+  }
+
+  def matToArray(m: ReadMat[_], array: Array[Double]) { matToArray(m, array, 0)}
+
+  /** Column major order.
+   *
+   */
+  def matToArray(m: ReadMat[_], array: Array[Double], offset: Int) {
+    array(offset + 0) = m.d00
+    array(offset + 1) = m.d10
+    array(offset + 2) = m.d20
+    array(offset + 3) = m.d30
+
+    array(offset + 4) = m.d01
+    array(offset + 5) = m.d11
+    array(offset + 6) = m.d21
+    array(offset + 7) = m.d31
+
+    array(offset + 8) = m.d02
+    array(offset + 9) = m.d12
+    array(offset + 10)= m.d22
+    array(offset + 11)= m.d32
+
+    array(offset + 12)= m.d03
+    array(offset + 13)= m.d13
+    array(offset + 14)= m.d23
+    array(offset + 15)= m.d33
+  }
+
+  def matToBuffer(m: ReadMat[_], buffer: DoubleBuffer) {
+    buffer.put(m.d00)
+    buffer.put(m.d10)
+    buffer.put(m.d20)
+    buffer.put(m.d30)
+
+    buffer.put(m.d01)
+    buffer.put(m.d11)
+    buffer.put(m.d21)
+    buffer.put(m.d31)
+
+    buffer.put(m.d02)
+    buffer.put(m.d12)
+    buffer.put(m.d22)
+    buffer.put(m.d32)
+
+    buffer.put(m.d03)
+    buffer.put(m.d13)
+    buffer.put(m.d23)
+    buffer.put(m.d33)
+  }
 }

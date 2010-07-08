76908f0197590920c5906e8e815be31a01077a35
lexn82 <lexn82@fe83b338-bbbe-11de-ad49-1f49b0b801a7>
2010-07-08 23:29:51 +0000

    Added Property Tests.

---
+++ Simplex3dMathTest/src/bench/property/PrimitivePropBench.scala
@@ -0,0 +1,298 @@
+/*
+ * Simplex3d, MathTest package
+ * Copyright (C) 2010, Simplex3d Team
+ *
+ * This file is part of Simplex3dMathTest.
+ *
+ * Simplex3dMathTest is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Simplex3dMathTest is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bench.property
+
+import simplex3d.math._
+
+
+/**
+ * @author Aleksey Nikiforov (lex)
+ */
+object PrimitivePropBench {
+
+  def main(args: Array[String]) {
+    testReadWrite()
+    testReadWrite()
+    testReadWrite()
+
+    testUpdate()
+    testUpdate()
+    testUpdate()
+  }
+
+  val len = 400*1000*1000
+
+  def testReadWrite() {
+    var start = 0L
+
+    start = System.currentTimeMillis
+    testVarReadWrite(len)
+    System.gc()
+    val varReadWriteTime = System.currentTimeMillis - start
+
+    start = System.currentTimeMillis
+    testSpecializedReadWrite(len)
+    System.gc()
+    val specReadWriteTime = System.currentTimeMillis - start
+
+    start = System.currentTimeMillis
+    testGenericReadWrite(len)
+    System.gc()
+    val genericReadWriteTime = System.currentTimeMillis - start
+
+    start = System.currentTimeMillis
+    testSpecializedReadWriteD(len)
+    System.gc()
+    val specReadWriteDTime = System.currentTimeMillis - start
+
+    start = System.currentTimeMillis
+    testGenericReadWriteD(len)
+    System.gc()
+    val genericReadWriteDTime = System.currentTimeMillis - start
+
+    println("Var Int r/w time: " + varReadWriteTime + ".")
+    println("Spec Int r/w time: " + specReadWriteTime + ".")
+    println("Generic Int r/w time: " + genericReadWriteTime + ".")
+    println("Spec Double r/w time: " + specReadWriteDTime + ".")
+    println("Generic Double r/w time: " + genericReadWriteDTime + ".")
+  }
+
+  final def testVarReadWrite(length: Int) {
+    var p = 0
+
+    var i = 0; while (i < length) {
+
+      // Bench code
+      val c = p
+      p = c + (c | i)
+
+      i += 1
+    }
+
+    println(p)
+  }
+
+  final def testSpecializedReadWrite(length: Int) {
+    val p: Property[Int] = new SpecializedProperty(
+      0,
+      (x: Int) => throw new AssertionError
+    )
+
+    var i = 0; while (i < length) {
+
+      // Bench code
+      val c = p()
+      p := c + (c | i)
+
+      i += 1
+    }
+
+    println(p)
+  }
+
+  final def testGenericReadWrite(length: Int) {
+    val p: Property[Int] = new GenericProperty(
+      0,
+      (x: Int) => throw new AssertionError
+    )
+
+    var i = 0; while (i < length) {
+
+      // Bench code
+      val c = p()
+      p := c + (c | i)
+
+      i += 1
+    }
+
+    println(p)
+  }
+
+  final def testSpecializedReadWriteD(length: Int) {
+    val p: Property[Double] = new SpecializedProperty(
+      0.0,
+      (x: Double) => throw new AssertionError
+    )
+
+    var i = 0; while (i < length) {
+
+      // Bench code
+      val c = p()
+      p := c + i
+
+      i += 1
+    }
+
+    println(p)
+  }
+
+  final def testGenericReadWriteD(length: Int) {
+    val p: Property[Double] = new GenericProperty(
+      0.0,
+      (x: Double) => throw new AssertionError
+    )
+
+    var i = 0; while (i < length) {
+
+      // Bench code
+      val c = p()
+      p := c + i
+
+      i += 1
+    }
+
+    println(p)
+  }
+
+
+  def testUpdate() {
+    var start = 0L
+
+    start = System.currentTimeMillis
+    testVarUpdate(len)
+    System.gc()
+    val varUpdateTime = System.currentTimeMillis - start
+
+    start = System.currentTimeMillis
+    testSpecializedUpdate(len)
+    System.gc()
+    val specUpdateTime = System.currentTimeMillis - start
+
+    start = System.currentTimeMillis
+    testGenericUpdate(len)
+    System.gc()
+    val genericUpdateTime = System.currentTimeMillis - start
+
+    start = System.currentTimeMillis
+    testSpecializedDUpdate(len)
+    System.gc()
+    val specUpdateDTime = System.currentTimeMillis - start
+
+    start = System.currentTimeMillis
+    testGenericDUpdate(len)
+    System.gc()
+    val genericUpdateDTime = System.currentTimeMillis - start
+
+    println("Var Int r/w time: " + varUpdateTime + ".")
+    println("Spec Int r/w time: " + specUpdateTime + ".")
+    println("Generic Int r/w time: " + genericUpdateTime + ".")
+    println("Spec Double r/w time: " + specUpdateDTime + ".")
+    println("Generic Double r/w time: " + genericUpdateDTime + ".")
+  }
+
+  final var count = 0
+  final val function = (x: Int) => { count += 1; x + count }
+  final val functionD = (x: Double) => { count += 1; x + count }
+
+  final def testVarUpdate(length: Int) {
+    count = 0
+    var p = 0
+
+    var i = 0; while (i < length) {
+
+      // Bench code
+      p = function(p)
+
+      i += 1
+    }
+
+    println(p)
+  }
+
+  final def testSpecializedUpdate(length: Int) {
+    count = 0
+    val p = new SpecializedProperty(0, function)
+
+    var i = 0; while (i < length) {
+
+      // Bench code
+      p.update()
+
+      i += 1
+    }
+
+    println(p)
+  }
+
+  final def testGenericUpdate(length: Int) {
+    count = 0
+    val p = new GenericProperty(0, function)
+
+    var i = 0; while (i < length) {
+
+      // Bench code
+      p.update()
+
+      i += 1
+    }
+
+    println(p)
+  }
+
+  final def testSpecializedDUpdate(length: Int) {
+    count = 0
+    val p = new SpecializedProperty(0.0, functionD)
+
+    var i = 0; while (i < length) {
+
+      // Bench code
+      p.update()
+
+      i += 1
+    }
+
+    println(p)
+  }
+
+  final def testGenericDUpdate(length: Int) {
+    count = 0
+    val p = new GenericProperty(0.0, functionD)
+
+    var i = 0; while (i < length) {
+
+      // Bench code
+      p.update()
+
+      i += 1
+    }
+
+    println(p)
+  }
+}
+
+final class GenericProperty[T](
+  initialValue: PropertyValue[T], function: (T) => T
+) extends Property[T] {
+  protected val value = initialValue.copyAsMutable()
+  def update() {
+    updateWith(function)
+  }
+}
+
+final class SpecializedProperty[@specialized(Boolean, Int, Float, Double) T](
+  initialValue: PropertyValue[T], function: (T) => T
+) extends Property[T] {
+  protected val value = initialValue.copyAsMutable()
+
+  def update() {
+    updateWith(function)
+  }
+}
---
+++ Simplex3dMathTest/src/test/math/PropertyTest.scala
@@ -0,0 +1,374 @@
+/*
+ * Simplex3d, MathTest package
+ * Copyright (C) 2010, Simplex3d Team
+ *
+ * This file is part of Simplex3dMathTest.
+ *
+ * Simplex3dMathTest is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Simplex3dMathTest is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package test.math
+
+import org.scalatest._
+
+import simplex3d.math._
+import simplex3d.math.intm._
+import simplex3d.math.floatm._
+import simplex3d.math.doublem._
+
+
+/**
+ * @author Aleksey Nikiforov (lex)
+ */
+class PropertyTest extends FunSuite {
+
+  class SimpleProperty[@specialized(Boolean, Int, Float, Double) T](
+    init: PropertyValue[T]
+  ) extends Property[T] {
+    protected val value = init.copyAsMutable()
+  }
+  
+  test("Properties") {
+    {
+      val p = new SimpleProperty(true)
+      assert(p() == true)
+      p := false
+      assert(p() == false)
+    }
+    {
+      val p = new SimpleProperty(2)
+      assert(p() == 2)
+      p := 3
+      assert(p() == 3)
+    }
+    {
+      val p = new SimpleProperty(2f)
+      assert(p() == 2f)
+      p := 3f
+      assert(p() == 3f)
+    }
+    {
+      val p = new SimpleProperty(2d)
+      assert(p() == 2d)
+      p := 3d
+      assert(p() == 3d)
+    }
+
+    {
+      val p = new SimpleProperty(Vec2b(true))
+      assert(p() == Vec2b(true))
+      p := Vec2b(false)
+      assert(p() == Vec2b(false))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Vec3b(true))
+      assert(p() == Vec3b(true))
+      p := Vec3b(false)
+      assert(p() == Vec3b(false))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Vec4b(true))
+      assert(p() == Vec4b(true))
+      p := Vec4b(false)
+      assert(p() == Vec4b(false))
+      checkType(p())
+    }
+
+    {
+      val p = new SimpleProperty(Vec2i(2))
+      assert(p() == Vec2i(2))
+      p := Vec2i(3)
+      assert(p() == Vec2i(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Vec3i(2))
+      assert(p() == Vec3i(2))
+      p := Vec3i(3)
+      assert(p() == Vec3i(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Vec4i(2))
+      assert(p() == Vec4i(2))
+      p := Vec4i(3)
+      assert(p() == Vec4i(3))
+      checkType(p())
+    }
+
+    {
+      val p = new SimpleProperty(Vec2f(2))
+      assert(p() == Vec2f(2))
+      p := Vec2f(3)
+      assert(p() == Vec2f(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Vec3f(2))
+      assert(p() == Vec3f(2))
+      p := Vec3f(3)
+      assert(p() == Vec3f(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Vec4f(2))
+      assert(p() == Vec4f(2))
+      p := Vec4f(3)
+      assert(p() == Vec4f(3))
+      checkType(p())
+    }
+
+    {
+      val p = new SimpleProperty(Vec2d(2))
+      assert(p() == Vec2d(2))
+      p := Vec2d(3)
+      assert(p() == Vec2d(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Vec3d(2))
+      assert(p() == Vec3d(2))
+      p := Vec3d(3)
+      assert(p() == Vec3d(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Vec4d(2))
+      assert(p() == Vec4d(2))
+      p := Vec4d(3)
+      assert(p() == Vec4d(3))
+      checkType(p())
+    }
+
+    {
+      val p = new SimpleProperty(Quat4f(2, 2, 2, 2))
+      assert(p() == Quat4f(2, 2, 2, 2))
+      p := Quat4f(3, 3, 3, 3)
+      assert(p() == Quat4f(3, 3, 3, 3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Quat4d(2, 2, 2, 2))
+      assert(p() == Quat4d(2, 2, 2, 2))
+      p := Quat4d(3, 3, 3, 3)
+      assert(p() == Quat4d(3, 3, 3, 3))
+      checkType(p())
+    }
+
+    {
+      val p = new SimpleProperty(Mat2x2f(2))
+      assert(p() == Mat2x2f(2))
+      p := Mat2x2f(3)
+      assert(p() == Mat2x2f(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat2x3f(2))
+      assert(p() == Mat2x3f(2))
+      p := Mat2x3f(3)
+      assert(p() == Mat2x3f(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat2x4f(2))
+      assert(p() == Mat2x4f(2))
+      p := Mat2x4f(3)
+      assert(p() == Mat2x4f(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat3x2f(2))
+      assert(p() == Mat3x2f(2))
+      p := Mat3x2f(3)
+      assert(p() == Mat3x2f(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat3x3f(2))
+      assert(p() == Mat3x3f(2))
+      p := Mat3x3f(3)
+      assert(p() == Mat3x3f(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat3x4f(2))
+      assert(p() == Mat3x4f(2))
+      p := Mat3x4f(3)
+      assert(p() == Mat3x4f(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat4x2f(2))
+      assert(p() == Mat4x2f(2))
+      p := Mat4x2f(3)
+      assert(p() == Mat4x2f(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat4x3f(2))
+      assert(p() == Mat4x3f(2))
+      p := Mat4x3f(3)
+      assert(p() == Mat4x3f(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat4x4f(2))
+      assert(p() == Mat4x4f(2))
+      p := Mat4x4f(3)
+      assert(p() == Mat4x4f(3))
+      checkType(p())
+    }
+
+    {
+      val p = new SimpleProperty(Mat2x2d(2))
+      assert(p() == Mat2x2d(2))
+      p := Mat2x2d(3)
+      assert(p() == Mat2x2d(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat2x3d(2))
+      assert(p() == Mat2x3d(2))
+      p := Mat2x3d(3)
+      assert(p() == Mat2x3d(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat2x4d(2))
+      assert(p() == Mat2x4d(2))
+      p := Mat2x4d(3)
+      assert(p() == Mat2x4d(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat3x2d(2))
+      assert(p() == Mat3x2d(2))
+      p := Mat3x2d(3)
+      assert(p() == Mat3x2d(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat3x3d(2))
+      assert(p() == Mat3x3d(2))
+      p := Mat3x3d(3)
+      assert(p() == Mat3x3d(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat3x4d(2))
+      assert(p() == Mat3x4d(2))
+      p := Mat3x4d(3)
+      assert(p() == Mat3x4d(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat4x2d(2))
+      assert(p() == Mat4x2d(2))
+      p := Mat4x2d(3)
+      assert(p() == Mat4x2d(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat4x3d(2))
+      assert(p() == Mat4x3d(2))
+      p := Mat4x3d(3)
+      assert(p() == Mat4x3d(3))
+      checkType(p())
+    }
+    {
+      val p = new SimpleProperty(Mat4x4d(2))
+      assert(p() == Mat4x4d(2))
+      p := Mat4x4d(3)
+      assert(p() == Mat4x4d(3))
+      checkType(p())
+    }
+  }
+
+  def checkType(value: AnyVec2b) {}
+  def checkType(value: Vec2b) { throw new AssertionError }
+  def checkType(value: AnyVec3b) {}
+  def checkType(value: Vec3b) { throw new AssertionError }
+  def checkType(value: AnyVec4b) {}
+  def checkType(value: Vec4b) { throw new AssertionError }
+
+  def checkType(value: AnyVec2i) {}
+  def checkType(value: Vec2i) { throw new AssertionError }
+  def checkType(value: AnyVec3i) {}
+  def checkType(value: Vec3i) { throw new AssertionError }
+  def checkType(value: AnyVec4i) {}
+  def checkType(value: Vec4i) { throw new AssertionError }
+
+  def checkType(value: AnyVec2f) {}
+  def checkType(value: Vec2f) { throw new AssertionError }
+  def checkType(value: AnyVec3f) {}
+  def checkType(value: Vec3f) { throw new AssertionError }
+  def checkType(value: AnyVec4f) {}
+  def checkType(value: Vec4f) { throw new AssertionError }
+
+  def checkType(value: AnyVec2d) {}
+  def checkType(value: Vec2d) { throw new AssertionError }
+  def checkType(value: AnyVec3d) {}
+  def checkType(value: Vec3d) { throw new AssertionError }
+  def checkType(value: AnyVec4d) {}
+  def checkType(value: Vec4d) { throw new AssertionError }
+
+  def checkType(value: AnyQuat4f) {}
+  def checkType(value: Quat4f) { throw new AssertionError }
+  def checkType(value: AnyQuat4d) {}
+  def checkType(value: Quat4d) { throw new AssertionError }
+
+  def checkType(value: AnyMat2x2f) {}
+  def checkType(value: Mat2x2f) { throw new AssertionError }
+  def checkType(value: AnyMat2x3f) {}
+  def checkType(value: Mat2x3f) { throw new AssertionError }
+  def checkType(value: AnyMat2x4f) {}
+  def checkType(value: Mat2x4f) { throw new AssertionError }
+
+  def checkType(value: AnyMat3x2f) {}
+  def checkType(value: Mat3x2f) { throw new AssertionError }
+  def checkType(value: AnyMat3x3f) {}
+  def checkType(value: Mat3x3f) { throw new AssertionError }
+  def checkType(value: AnyMat3x4f) {}
+  def checkType(value: Mat3x4f) { throw new AssertionError }
+
+  def checkType(value: AnyMat4x2f) {}
+  def checkType(value: Mat4x2f) { throw new AssertionError }
+  def checkType(value: AnyMat4x3f) {}
+  def checkType(value: Mat4x3f) { throw new AssertionError }
+  def checkType(value: AnyMat4x4f) {}
+  def checkType(value: Mat4x4f) { throw new AssertionError }
+
+  def checkType(value: AnyMat2x2d) {}
+  def checkType(value: Mat2x2d) { throw new AssertionError }
+  def checkType(value: AnyMat2x3d) {}
+  def checkType(value: Mat2x3d) { throw new AssertionError }
+  def checkType(value: AnyMat2x4d) {}
+  def checkType(value: Mat2x4d) { throw new AssertionError }
+
+  def checkType(value: AnyMat3x2d) {}
+  def checkType(value: Mat3x2d) { throw new AssertionError }
+  def checkType(value: AnyMat3x3d) {}
+  def checkType(value: Mat3x3d) { throw new AssertionError }
+  def checkType(value: AnyMat3x4d) {}
+  def checkType(value: Mat3x4d) { throw new AssertionError }
+
+  def checkType(value: AnyMat4x2d) {}
+  def checkType(value: Mat4x2d) { throw new AssertionError }
+  def checkType(value: AnyMat4x3d) {}
+  def checkType(value: Mat4x3d) { throw new AssertionError }
+  def checkType(value: AnyMat4x4d) {}
+  def checkType(value: Mat4x4d) { throw new AssertionError }
+}

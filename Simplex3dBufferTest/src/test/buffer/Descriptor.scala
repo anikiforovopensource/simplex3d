ba017670384a291bbfbc2a9c68b1f017eea9f3b9
lexn82 <lexn82@fe83b338-bbbe-11de-ad49-1f49b0b801a7>
2010-09-07 21:03:43 +0000

    Testing framework: work in progress.

--- Simplex3dBufferTest/src/test/buffer/Descriptor.scala
+++ Simplex3dBufferTest/src/test/buffer/Descriptor.scala
@@ -20,16 +20,128 @@
 
 package test.buffer
 
+import scala.reflect._
 import simplex3d.buffer._
+import simplex3d.math.intm._
+import simplex3d.math.floatm._
+import simplex3d.math.doublem._
 
 
 /**
  * @author Aleksey Nikiforov (lex)
  */
-case class Descriptor(
+case class Descriptor[E <: MetaElement, R <: RawData](
   elementManifest: Manifest[_],
   componentManifest: Manifest[_],
   components: Int,
   rawType: Int,
   normalized: Boolean
 )
+
+object Descriptors {
+  implicit val Int1SByte = Descriptor[Int1, SByte](Manifest.Int, Manifest.Int, 1, RawData.SByte, false)
+  implicit val Int1UByte = Descriptor[Int1, UByte](Manifest.Int, Manifest.Int, 1, RawData.UByte, false)
+  implicit val Int1SShort = Descriptor[Int1, SShort](Manifest.Int, Manifest.Int, 1, RawData.SShort, false)
+  implicit val Int1UShort = Descriptor[Int1, UShort](Manifest.Int, Manifest.Int, 1, RawData.UShort, false)
+  implicit val Int1SInt = Descriptor[Int1, SByte](Manifest.Int, Manifest.Int, 1, RawData.SInt, false)
+  implicit val Int1UInt = Descriptor[Int1, SByte](Manifest.Int, Manifest.Int, 1, RawData.UInt, false)
+  
+  implicit val Vec2iSByte = Descriptor[Vec2i, SByte](Vec2i.Manifest, Manifest.Int, 2, RawData.SByte, false)
+  implicit val Vec2iUByte = Descriptor[Vec2i, UByte](Vec2i.Manifest, Manifest.Int, 2, RawData.UByte, false)
+  implicit val Vec2iSShort = Descriptor[Vec2i, SShort](Vec2i.Manifest, Manifest.Int, 2, RawData.SShort, false)
+  implicit val Vec2iUShort = Descriptor[Vec2i, UShort](Vec2i.Manifest, Manifest.Int, 2, RawData.UShort, false)
+  implicit val Vec2iSInt = Descriptor[Vec2i, SByte](Vec2i.Manifest, Manifest.Int, 2, RawData.SInt, false)
+  implicit val Vec2iUInt = Descriptor[Vec2i, SByte](Vec2i.Manifest, Manifest.Int, 2, RawData.UInt, false)
+
+  implicit val Vec3iSByte = Descriptor[Vec3i, SByte](Vec3i.Manifest, Manifest.Int, 3, RawData.SByte, false)
+  implicit val Vec3iUByte = Descriptor[Vec3i, UByte](Vec3i.Manifest, Manifest.Int, 3, RawData.UByte, false)
+  implicit val Vec3iSShort = Descriptor[Vec3i, SShort](Vec3i.Manifest, Manifest.Int, 3, RawData.SShort, false)
+  implicit val Vec3iUShort = Descriptor[Vec3i, UShort](Vec3i.Manifest, Manifest.Int, 3, RawData.UShort, false)
+  implicit val Vec3iSInt = Descriptor[Vec3i, SByte](Vec3i.Manifest, Manifest.Int, 3, RawData.SInt, false)
+  implicit val Vec3iUInt = Descriptor[Vec3i, SByte](Vec3i.Manifest, Manifest.Int, 3, RawData.UInt, false)
+
+  implicit val Vec4iSByte = Descriptor[Vec4i, SByte](Vec4i.Manifest, Manifest.Int, 4, RawData.SByte, false)
+  implicit val Vec4iUByte = Descriptor[Vec4i, UByte](Vec4i.Manifest, Manifest.Int, 4, RawData.UByte, false)
+  implicit val Vec4iSShort = Descriptor[Vec4i, SShort](Vec4i.Manifest, Manifest.Int, 4, RawData.SShort, false)
+  implicit val Vec4iUShort = Descriptor[Vec4i, UShort](Vec4i.Manifest, Manifest.Int, 4, RawData.UShort, false)
+  implicit val Vec4iSInt = Descriptor[Vec4i, SByte](Vec4i.Manifest, Manifest.Int, 4, RawData.SInt, false)
+  implicit val Vec4iUInt = Descriptor[Vec4i, SByte](Vec4i.Manifest, Manifest.Int, 4, RawData.UInt, false)
+  
+  
+  implicit val Float1SByte = Descriptor[Float1, SByte](Manifest.Float, Manifest.Float, 1, RawData.SByte, true)
+  implicit val Float1UByte = Descriptor[Float1, UByte](Manifest.Float, Manifest.Float, 1, RawData.UByte, true)
+  implicit val Float1SShort = Descriptor[Float1, SShort](Manifest.Float, Manifest.Float, 1, RawData.SShort, true)
+  implicit val Float1UShort = Descriptor[Float1, UShort](Manifest.Float, Manifest.Float, 1, RawData.UShort, true)
+  implicit val Float1SInt = Descriptor[Float1, SByte](Manifest.Float, Manifest.Float, 1, RawData.SInt, true)
+  implicit val Float1UInt = Descriptor[Float1, SByte](Manifest.Float, Manifest.Float, 1, RawData.UInt, true)
+  implicit val Float1HalfFloat = Descriptor[Float1, HalfFloat](Manifest.Float, Manifest.Float, 1, RawData.HalfFloat, false)
+  implicit val Float1RawFloat = Descriptor[Float1, RawFloat](Manifest.Float, Manifest.Float, 1, RawData.RawFloat, false)
+  
+  implicit val Vec2fSByte = Descriptor[Vec2f, SByte](Vec2f.Manifest, Manifest.Float, 2, RawData.SByte, true)
+  implicit val Vec2fUByte = Descriptor[Vec2f, UByte](Vec2f.Manifest, Manifest.Float, 2, RawData.UByte, true)
+  implicit val Vec2fSShort = Descriptor[Vec2f, SShort](Vec2f.Manifest, Manifest.Float, 2, RawData.SShort, true)
+  implicit val Vec2fUShort = Descriptor[Vec2f, UShort](Vec2f.Manifest, Manifest.Float, 2, RawData.UShort, true)
+  implicit val Vec2fSInt = Descriptor[Vec2f, SByte](Vec2f.Manifest, Manifest.Float, 2, RawData.SInt, true)
+  implicit val Vec2fUInt = Descriptor[Vec2f, SByte](Vec2f.Manifest, Manifest.Float, 2, RawData.UInt, true)
+  implicit val Vec2fHalfFloat = Descriptor[Vec2f, HalfFloat](Vec2f.Manifest, Manifest.Float, 2, RawData.HalfFloat, false)
+  implicit val Vec2fRawFloat = Descriptor[Vec2f, RawFloat](Vec2f.Manifest, Manifest.Float, 2, RawData.RawFloat, false)
+  
+  implicit val Vec3fSByte = Descriptor[Vec3f, SByte](Vec3f.Manifest, Manifest.Float, 3, RawData.SByte, true)
+  implicit val Vec3fUByte = Descriptor[Vec3f, UByte](Vec3f.Manifest, Manifest.Float, 3, RawData.UByte, true)
+  implicit val Vec3fSShort = Descriptor[Vec3f, SShort](Vec3f.Manifest, Manifest.Float, 3, RawData.SShort, true)
+  implicit val Vec3fUShort = Descriptor[Vec3f, UShort](Vec3f.Manifest, Manifest.Float, 3, RawData.UShort, true)
+  implicit val Vec3fSInt = Descriptor[Vec3f, SByte](Vec3f.Manifest, Manifest.Float, 3, RawData.SInt, true)
+  implicit val Vec3fUInt = Descriptor[Vec3f, SByte](Vec3f.Manifest, Manifest.Float, 3, RawData.UInt, true)
+  implicit val Vec3fHalfFloat = Descriptor[Vec3f, HalfFloat](Vec3f.Manifest, Manifest.Float, 3, RawData.HalfFloat, false)
+  implicit val Vec3fRawFloat = Descriptor[Vec3f, RawFloat](Vec3f.Manifest, Manifest.Float, 3, RawData.RawFloat, false)
+  
+  implicit val Vec4fSByte = Descriptor[Vec4f, SByte](Vec4f.Manifest, Manifest.Float, 4, RawData.SByte, true)
+  implicit val Vec4fUByte = Descriptor[Vec4f, UByte](Vec4f.Manifest, Manifest.Float, 4, RawData.UByte, true)
+  implicit val Vec4fSShort = Descriptor[Vec4f, SShort](Vec4f.Manifest, Manifest.Float, 4, RawData.SShort, true)
+  implicit val Vec4fUShort = Descriptor[Vec4f, UShort](Vec4f.Manifest, Manifest.Float, 4, RawData.UShort, true)
+  implicit val Vec4fSInt = Descriptor[Vec4f, SByte](Vec4f.Manifest, Manifest.Float, 4, RawData.SInt, true)
+  implicit val Vec4fUInt = Descriptor[Vec4f, SByte](Vec4f.Manifest, Manifest.Float, 4, RawData.UInt, true)
+  implicit val Vec4fHalfFloat = Descriptor[Vec4f, HalfFloat](Vec4f.Manifest, Manifest.Float, 4, RawData.HalfFloat, false)
+  implicit val Vec4fRawFloat = Descriptor[Vec4f, RawFloat](Vec4f.Manifest, Manifest.Float, 4, RawData.RawFloat, false)
+  
+  
+  implicit val Double1SByte = Descriptor[Double1, SByte](Manifest.Double, Manifest.Double, 1, RawData.SByte, true)
+  implicit val Double1UByte = Descriptor[Double1, UByte](Manifest.Double, Manifest.Double, 1, RawData.UByte, true)
+  implicit val Double1SShort = Descriptor[Double1, SShort](Manifest.Double, Manifest.Double, 1, RawData.SShort, true)
+  implicit val Double1UShort = Descriptor[Double1, UShort](Manifest.Double, Manifest.Double, 1, RawData.UShort, true)
+  implicit val Double1SInt = Descriptor[Double1, SByte](Manifest.Double, Manifest.Double, 1, RawData.SInt, true)
+  implicit val Double1UInt = Descriptor[Double1, SByte](Manifest.Double, Manifest.Double, 1, RawData.UInt, true)
+  implicit val Double1HalfFloat = Descriptor[Double1, HalfFloat](Manifest.Double, Manifest.Double, 1, RawData.HalfFloat, false)
+  implicit val Double1RawFloat = Descriptor[Double1, RawFloat](Manifest.Double, Manifest.Double, 1, RawData.RawFloat, false)
+  implicit val Double1RawDouble = Descriptor[Double1, RawDouble](Manifest.Double, Manifest.Double, 1, RawData.RawDouble, false)
+  
+  implicit val Vec2dSByte = Descriptor[Vec2d, SByte](Vec2d.Manifest, Manifest.Double, 2, RawData.SByte, true)
+  implicit val Vec2dUByte = Descriptor[Vec2d, UByte](Vec2d.Manifest, Manifest.Double, 2, RawData.UByte, true)
+  implicit val Vec2dSShort = Descriptor[Vec2d, SShort](Vec2d.Manifest, Manifest.Double, 2, RawData.SShort, true)
+  implicit val Vec2dUShort = Descriptor[Vec2d, UShort](Vec2d.Manifest, Manifest.Double, 2, RawData.UShort, true)
+  implicit val Vec2dSInt = Descriptor[Vec2d, SByte](Vec2d.Manifest, Manifest.Double, 2, RawData.SInt, true)
+  implicit val Vec2dUInt = Descriptor[Vec2d, SByte](Vec2d.Manifest, Manifest.Double, 2, RawData.UInt, true)
+  implicit val Vec2dHalfFloat = Descriptor[Vec2d, HalfFloat](Vec2d.Manifest, Manifest.Double, 2, RawData.HalfFloat, false)
+  implicit val Vec2dRawFloat = Descriptor[Vec2d, RawFloat](Vec2d.Manifest, Manifest.Double, 2, RawData.RawFloat, false)
+  implicit val Vec2dRawDouble = Descriptor[Vec2d, RawDouble](Vec2d.Manifest, Manifest.Double, 2, RawData.RawDouble, false)
+  
+  implicit val Vec3dSByte = Descriptor[Vec3d, SByte](Vec3d.Manifest, Manifest.Double, 3, RawData.SByte, true)
+  implicit val Vec3dUByte = Descriptor[Vec3d, UByte](Vec3d.Manifest, Manifest.Double, 3, RawData.UByte, true)
+  implicit val Vec3dSShort = Descriptor[Vec3d, SShort](Vec3d.Manifest, Manifest.Double, 3, RawData.SShort, true)
+  implicit val Vec3dUShort = Descriptor[Vec3d, UShort](Vec3d.Manifest, Manifest.Double, 3, RawData.UShort, true)
+  implicit val Vec3dSInt = Descriptor[Vec3d, SByte](Vec3d.Manifest, Manifest.Double, 3, RawData.SInt, true)
+  implicit val Vec3dUInt = Descriptor[Vec3d, SByte](Vec3d.Manifest, Manifest.Double, 3, RawData.UInt, true)
+  implicit val Vec3dHalfFloat = Descriptor[Vec3d, HalfFloat](Vec3d.Manifest, Manifest.Double, 3, RawData.HalfFloat, false)
+  implicit val Vec3dRawFloat = Descriptor[Vec3d, RawFloat](Vec3d.Manifest, Manifest.Double, 3, RawData.RawFloat, false)
+  implicit val Vec3dRawDouble = Descriptor[Vec3d, RawDouble](Vec3d.Manifest, Manifest.Double, 3, RawData.RawDouble, false)
+  
+  implicit val Vec4dSByte = Descriptor[Vec4d, SByte](Vec4d.Manifest, Manifest.Double, 4, RawData.SByte, true)
+  implicit val Vec4dUByte = Descriptor[Vec4d, UByte](Vec4d.Manifest, Manifest.Double, 4, RawData.UByte, true)
+  implicit val Vec4dSShort = Descriptor[Vec4d, SShort](Vec4d.Manifest, Manifest.Double, 4, RawData.SShort, true)
+  implicit val Vec4dUShort = Descriptor[Vec4d, UShort](Vec4d.Manifest, Manifest.Double, 4, RawData.UShort, true)
+  implicit val Vec4dSInt = Descriptor[Vec4d, SByte](Vec4d.Manifest, Manifest.Double, 4, RawData.SInt, true)
+  implicit val Vec4dUInt = Descriptor[Vec4d, SByte](Vec4d.Manifest, Manifest.Double, 4, RawData.UInt, true)
+  implicit val Vec4dHalfFloat = Descriptor[Vec4d, HalfFloat](Vec4d.Manifest, Manifest.Double, 4, RawData.HalfFloat, false)
+  implicit val Vec4dRawFloat = Descriptor[Vec4d, RawFloat](Vec4d.Manifest, Manifest.Double, 4, RawData.RawFloat, false)
+  implicit val Vec4dRawDouble = Descriptor[Vec4d, RawDouble](Vec4d.Manifest, Manifest.Double, 4, RawData.RawDouble, false)
+}
--- Simplex3dBufferTest/src/test/buffer/TestUtil.scala
+++ Simplex3dBufferTest/src/test/buffer/TestUtil.scala
@@ -29,7 +29,7 @@ import simplex3d.buffer._
  */
 object TestUtil {
 
-  private def rawLengh(rawType: Int) :Int = {
+  private def rawLength(rawType: Int) :Int = {
     import RawData._
     rawType match {
       case SByte => 1
@@ -44,65 +44,241 @@ object TestUtil {
     }
   }
 
-  def testSeq(
-    seq: DataSeq[_ <: MetaElement, RawData],
-    descriptor: Descriptor,
-    data: Buffer
+  private def checkBuffer(testing: Buffer, data: Buffer) {
+    assert(testing ne data)
+
+    assert((testing.position, data.position) == (0, 0))
+    assert(testing.capacity == data.capacity)
+    assert((testing.limit, data.limit) == (testing.capacity, testing.capacity))
+    assert(testing.equals(data))
+
+    (testing, data) match {
+      case (b1: ByteBuffer, b2: ByteBuffer) =>
+      case (b1: ShortBuffer, b2: ShortBuffer) =>
+      case (b1: CharBuffer, b2: CharBuffer) =>
+      case (b1: IntBuffer, b2: IntBuffer) =>
+      case (b1: FloatBuffer, b2: FloatBuffer) =>
+      case (b1: DoubleBuffer, b2: DoubleBuffer) =>
+    }
+  }
+
+  private def checkBindingBuffer(offset: Int, binding: Buffer, data: Buffer) {
+    data.position(offset)
+
+    assert(binding ne data)
+    assert(binding.capacity == binding.limit)
+
+    binding match {
+      case b: ByteBuffer => {
+        data match {
+          case d: ByteBuffer => assert(b.position == offset); assert(b equals data)
+          case d: ShortBuffer => assert(b.position == offset*2); assert(b.asShortBuffer equals data)
+          case d: CharBuffer => assert(b.position == offset*2); assert(b.asCharBuffer equals data)
+          case d: IntBuffer => assert(b.position == offset*4); assert(b.asIntBuffer equals data)
+          case d: FloatBuffer => assert(b.position == offset*4); assert(b.asFloatBuffer equals data)
+          case d: DoubleBuffer => assert(b.position == offset*8); assert(b.asDoubleBuffer equals data)
+        }
+      }
+      case b: ShortBuffer => assert(b.position == offset); assert(b equals data)
+      case b: CharBuffer => assert(b.position == offset); assert(b equals data)
+      case b: IntBuffer => assert(b.position == offset); assert(b equals data)
+      case b: FloatBuffer => assert(b.position == offset); assert(b equals data)
+      case b: DoubleBuffer => assert(b.position == offset); assert(b equals data)
+    }
+
+    data.position(0)
+  }
+
+  private def wrapArray(array: AnyRef) :Buffer = {
+    array match {
+      case a: Array[Byte] => ByteBuffer.wrap(a)
+      case a: Array[Short] => ShortBuffer.wrap(a)
+      case a: Array[Char] => CharBuffer.wrap(a)
+      case a: Array[Int] => IntBuffer.wrap(a)
+      case a: Array[Float] => FloatBuffer.wrap(a)
+      case a: Array[Double] => DoubleBuffer.wrap(a)
+    }
+  }
+
+  private def writeIntoArray(array: AnyRef) {
+    array match {
+      case a: Array[Byte] => a(0) = 1
+      case a: Array[Short] => a(0) = 1
+      case a: Array[Char] => a(0) = 1
+      case a: Array[Int] => a(0) = 1
+      case a: Array[Float] => a(0) = 1
+      case a: Array[Double] => a(0) = 1
+    }
+  }
+  
+  private def testSeq[E <: MetaElement, R <: RawData](
+    seq: ReadDataSeq[E, R],
+    readOnly: Boolean,
+    data: Buffer,
+    descriptor: Descriptor[E, R]
   ) {
-    assert(seq.elemManifest == descriptor.elementManifest)
+    assert(seq.elementManifest == descriptor.elementManifest)
     assert(seq.componentManifest == descriptor.componentManifest)
     assert(seq.components == descriptor.components)
     assert(seq.rawType == descriptor.rawType)
     assert(seq.normalized == descriptor.normalized)
+    assert(seq.isReadOnly == readOnly)
 
-    assert(rawLength(seq.rawType) == seq.bytesPerRawComponent)
-    assert(seq.bytesPerRawComponent*seq.asBuffer.limit == byteSize)
-    assert(seq.bytesPerRawComponent*seq.offset == seq.byteOffet)
-    assert(seq.bytesPerRawComponent*seq.stride == seq.byteStride)
-    
-    /*
-    asReadOnlyBuffer
-    sharesStoreObject
+    checkBuffer(seq.asReadOnlyBuffer, data)
+    assert(seq.asReadOnlyBuffer.isReadOnly)
 
-    bindingBuffer(offset: Int)
+    assert(seq.bytesPerRawComponent == rawLength(seq.rawType))
+    assert(seq.byteSize == seq.bytesPerRawComponent*data.limit)
+    assert(seq.byteOffset == seq.bytesPerRawComponent*seq.offset)
+    assert(seq.byteStride == seq.bytesPerRawComponent*seq.stride)
 
-    size
-    length == size
+    assert(seq.size == (data.limit - seq.offset)/seq.stride)
+    assert(seq.size == seq.length)
 
-    backingSeq
-    isReadOnly()
-    asReadOnlySeq()
-    asBuffer()
-    */
+    if (seq.isReadOnly) {
+      assert(seq.backingSeq.isReadOnly)
+      assert(seq.asReadOnlySeq eq seq)
+
+      if (!seq.isInstanceOf[DataArray[_, _]]) {
+        assert(seq.bindingBuffer(0).isReadOnly)
+      }
+    }
+    else {
+      assert(!seq.backingSeq.isReadOnly)
+      assert(seq.asReadOnlySeq ne seq)
+    }
+
+    if (seq.isInstanceOf[DataArray[_, _]]) {
+      assert(seq.bindingBuffer(0).array != null)
+    }
+
+    checkBindingBuffer(0, seq.bindingBuffer(0), data)
+    if (data.limit >= 2) {
+      checkBindingBuffer(1, seq.bindingBuffer(1), data)
+      checkBindingBuffer(2, seq.bindingBuffer(2), data)
+    }
+
+    val ds = seq.asInstanceOf[DataSeq[E, R]]
+    checkBuffer(ds.asBuffer, data)
+    assert(ds.asBuffer.isReadOnly == readOnly)
+  }
+
+  def testArray[E <: MetaElement, R <: RawData](
+    seq: ReadDataArray[E, R],
+    readOnly: Boolean,
+    data: Buffer
+  )(implicit descriptor: Descriptor[E, R]) {
+    assert(seq.offset == 0)
+    assert(seq.stride == seq.components)
+
+    assert(!seq.asReadOnlyBuffer.isDirect)
+    assert(seq.backingSeq.isInstanceOf[ReadDataArray[_, _]])
+    assert(seq.asReadOnlySeq.isInstanceOf[ReadDataArray[_, _]])
+
+    val ds = seq.asInstanceOf[DataArray[E, R]]
+    assert(!ds.asBuffer.isDirect)
+
+    if (!readOnly) {
+      assert(ds.backingSeq.isInstanceOf[DataArray[_, _]])
+
+      assert(ds.array != null)
+      checkBuffer(wrapArray(ds.array), data)
+    }
+    else {
+      try {
+        writeIntoArray(ds.array)
+        throw new AssertionError()
+      }
+      catch {
+        case e: Exception => // expected, do nothing
+      }
+    }
+
+    testSeq(seq, readOnly, data, descriptor)
+
+    // backingSeq
+    if (seq.components == 1) {
+      assert(seq eq seq.backingSeq)
+    }
+    else {
+      val backingDesc = descriptor.copy(elementManifest = descriptor.componentManifest, components = 1)
+      testArray(seq.backingSeq, readOnly, data)(backingDesc.asInstanceOf[Descriptor[E#Component, R]])
+    }
+
+    //asReadOnlySeq
+    if (!readOnly) testArray(seq.asReadOnlySeq, true, data)
+  }
+
+  def testBuffer[E <: MetaElement, R <: RawData](
+    seq: ReadDataBuffer[E, R],
+    readOnly: Boolean,
+    data: Buffer
+  )(implicit descriptor: Descriptor[E, R]) {
+    assert(seq.offset == 0)
+    assert(seq.stride == seq.components)
+
+    assert(seq.asReadOnlyBuffer.isDirect)
+    assert(seq.backingSeq.isInstanceOf[ReadDataBuffer[_, _]])
+    assert(seq.asReadOnlySeq.isInstanceOf[ReadDataBuffer[_, _]])
+
+    val ds = seq.asInstanceOf[DataBuffer[E, R]]
+    assert(ds.asBuffer.isDirect)
+
+    if (!readOnly) {
+      assert(ds.backingSeq.isInstanceOf[DataBuffer[_, _]])
+    }
+
+    testSeq(seq, readOnly, data, descriptor)
+
+    // backingSeq
+    if (seq.components == 1) {
+      assert(seq eq seq.backingSeq)
+    }
+    else {
+      val backingDesc = descriptor.copy(elementManifest = descriptor.componentManifest, components = 1)
+      testBuffer(seq.backingSeq, readOnly, data)(backingDesc.asInstanceOf[Descriptor[E#Component, R]])
+    }
+
+    //asReadOnlySeq
+    if (!readOnly) testBuffer(seq.asReadOnlySeq, true, data)
+  }
+
+  def testView[E <: MetaElement, R <: RawData](
+    seq: ReadDataView[E, R],
+    offset: Int,
+    stride: Int,
+    readOnly: Boolean,
+    data: Buffer
+  )(implicit descriptor: Descriptor[E, R]) {
+    assert(seq.offset == offset)
+    assert(seq.stride == stride)
+
+    assert(seq.asReadOnlyBuffer.isDirect)
+    assert(seq.backingSeq.isInstanceOf[ReadDataBuffer[_, _]])
+    assert(seq.asReadOnlySeq.isInstanceOf[ReadDataView[_, _]])
+
+    val ds = seq.asInstanceOf[DataView[E, R]]
+    assert(ds.asBuffer.isDirect)
+
+    if (!readOnly) {
+      assert(ds.backingSeq.isInstanceOf[DataBuffer[_, _]])
+    }
+
+    testSeq(seq, readOnly, data, descriptor)
+
+    // backingSeq
+    val backingDesc = descriptor.copy(elementManifest = descriptor.componentManifest, components = 1)
+    testBuffer(seq.backingSeq, readOnly, data)(backingDesc.asInstanceOf[Descriptor[E#Component, R]])
+
+    //asReadOnlySeq
+    if (!readOnly) testView(seq.asReadOnlySeq, offset, stride, true, data)
   }
   
 /*
-// TestArray
-  backingSeq
-  asReadOnlySeq()
-  array
-  offset
-  stride
-
-// TestBuffer
-  backingSeq
-  asReadOnlySeq()
-  offset
-  stride
-
-// TestView
-  backingSeq
-  asReadOnlySeq()
-  offset
-  stride
-
-// Test applyUpdate
-  apply(i: Int)
-  update(i: Int, v: S)
-
 // Test Factory
   test object factories;
   test Read object factories;
+  varargFactories
 
   mkDataArray(size: Int)
   mkDataArray(array: R#ArrayType)
@@ -111,6 +287,13 @@ object TestUtil {
   mkDataView(byteBuffer: ByteBuffer, offset: Int,stride: Int)
   mkReadDataBuffer(byteBuffer: ByteBuffer)
   mkReadDataView(byteBuffer: ByteBuffer, offset: Int,stride: Int)
+  sharesStoreObject
+
+  test Sequence Cast
+  
+// Test applyUpdate
+  apply(i: Int)
+  update(i: Int, v: S)
 
 // Test Copy
   copyAsDataArray()

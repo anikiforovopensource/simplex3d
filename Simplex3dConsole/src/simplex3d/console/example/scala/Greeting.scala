5da68aaf1d75f3413515a91db7e68bfaac4c0795
lexn82 <lexn82@fe83b338-bbbe-11de-ad49-1f49b0b801a7>
2011-03-06 20:34:13 +0000

    Fixed release-web build. Change local.jnlp to use relative path.

--- Simplex3dConsole/src/simplex3d/console/example/scala/Greeting.scala
+++ Simplex3dConsole/src/simplex3d/console/example/scala/Greeting.scala
@@ -6,4 +6,7 @@ object Greeting extends Application {
   val words = Array("Welcome", "to", "Interactive", "Scala", "Console")
   println(words.mkString(" ") + "!")
 
+  // Please share your examples!
+  // You can contribute on this page:
+  // http://simplex3d.org/forum/index.php?topic=9.0
 }

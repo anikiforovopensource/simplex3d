85e5c4177472fa814512de36f8fe7f7052cc801b
lexn82 <lexn82@fe83b338-bbbe-11de-ad49-1f49b0b801a7>
2011-05-20 23:16:37 +0000

    Added line examples and a tutorial on OpenGL coordinate spaces with some more examples in 3d.

---
+++ Simplex3dConsole/src/example/simplex3d/lines/CoordinateSpaces.scala
@@ -0,0 +1,139 @@
+package example.simplex3d.lines
+
+import simplex3d.math._
+import simplex3d.math.double._
+import simplex3d.math.doublex.functions._
+import simplex3d.data._
+import simplex3d.data.double._
+import simplex3d.console.extension.ImageUtils._
+
+
+/**
+ * @author Aleksey Nikiforov (lex)
+ */
+object CoordinateSpaces extends App {
+
+{
+  // Define axis so we have something to render.
+  val axisLength = 4.0
+  val axis = List(
+    Vec3(0, 0, 0), Vec3(axisLength, 0, 0),
+    Vec3(0, 0, 0), Vec3(0, axisLength, 0),
+    Vec3(0, 0, 0), Vec3(0, 0, axisLength)
+  )
+
+  // Define axis labels.
+  val h = axisLength*0.1 // Letter height.
+  val x = Vec3(axisLength + h, h, h) // X-label offset
+  val y = Vec3(h, axisLength + h, h) // Y-label offset
+  val z = Vec3(h, h, axisLength + h) // Z-label offset
+  val labels = List(
+    // X.
+    x + Vec3(-h, h, 0), x + Vec3(h, -h, 0),
+    x + Vec3(h, h, 0), x + Vec3(-h, -h, 0),
+    // Y.
+    y + Vec3(-h, h, 0), y + Vec3(0),
+    y + Vec3(h, h, 0), y + Vec3(0),
+    y + Vec3(0), y + Vec3(0, -h, 0),
+    // Z.
+    z + Vec3(-h, h, 0), z + Vec3(h, h, 0),
+    z + Vec3(h, h, 0), z + Vec3(-h, -h, 0),
+    z + Vec3(-h, -h, 0), z + Vec3(h, -h, 0)
+  )
+  
+  val lines = DataArray[Vec3, RFloat]((axis ::: labels): _*)
+  val linesOut = DataArray[Vec2, RFloat](lines.size)
+  
+  val colors = DataArray[Vec3, UByte](lines.size)
+  for (i <- 0 until colors.size/2) { colors(i*2) = Vec3(0, 1, 0); colors(i*2 + 1) = Vec3(0, 0, 1) }
+
+
+  /* Lines are transformed to window coordinates as follows:
+   *
+   * - Find the CameraMatrix that positions camera in world coordinates.
+   *   Invert it to find the ViewMatrix which transforms world
+   *   coordinates to view coordinates.
+   *   
+   * - Find the ModelMatrix, which positions the model in world coordinates.
+   *   This matrix will transform model coordinates to view coordinates.
+   * 
+   * - Mutliply the ViewMatrix by the ModelMatrix to produce ModelViewMatrix,
+   *   which transforms ModelCoordinates to view coordinates.
+   *   
+   * - Find the ProjectionMatrix, which transforms view coordinates to
+   *   ClipCoordinates. ProjectionMatrix can be set as perspective projection
+   *   or ortho projection.
+   *   
+   * - Multiply the ProjectionMatrix by the ModelViewMatrix to produce
+   *   ModelViewProjectionMatrix. This matrix transforms ModelCoordinates to
+   *   ClipCoordinates.
+   *   
+   * - Extend ModelCoordinates to HomogeneousCoordinates by taking
+   *   x, y, and z and setting w to 1.
+   *   
+   * - Multiply the ModelViewProjectionMatrix by the HomogeneousCoordinates
+   *   to produce ClipCoordinates.
+   *   
+   * - ClipCoordinates with XYZ components outside the range (-W, W) lie outside
+   *   the view frustrum and should be clipped.
+   *   
+   * - Perform PerspectiveDivision on ClippedCoordinates to produce
+   *   NormalizedDeviceCoordinates. NormalizedDeviceCoordinates are within
+   *   a cube where each component is in range (-1, 1).
+   *   
+   * - Scale and translate the NormalizedDeviceCoordinates according to
+   *   the Viewport parameters to produce the WindowCoordinates.
+   */
+  animateLines("Axis") { (dims, time) =>
+    val camRotationSpeed = radians(30) // 30 degrees per second
+    val camRotation = Quat4 rotateX(radians(-20)) rotateY(camRotationSpeed*time)
+    val camTranslation = camRotation.rotateVector(Vec3(0, 0, 20))
+
+    val cameraMatrix = Mat3x4 rotate(camRotation) translate(camTranslation)
+    val viewMatrix = inverse(cameraMatrix)
+
+    val modelMatrix = Mat3x4 Identity
+    val modelViewMatrix = modelMatrix.concat(viewMatrix)
+        
+    val projectionMatrix = perspectiveProj(
+      radians(45), // view angle
+      dims.x.toDouble/dims.y, // aspect ratio
+      10, 100 // near and far planes
+    )
+
+    val modelViewProjectionMatrix = projectionMatrix*Mat4(modelViewMatrix)
+        
+    val viewportScale = dims*0.5
+    val viewportOffset = dims*0.5
+          
+    var i = 0; while (i < lines.size/2) {
+      val j = i*2
+      
+      val modelCoordinates0 = lines(j)
+      val modelCoordinates1 = lines(j + 1)
+
+      val homogeneousCoordinates0 = Vec4(modelCoordinates0, 1)
+      val homogeneousCoordinates1 = Vec4(modelCoordinates1, 1)
+      
+      val clipCoordinates0 = modelViewProjectionMatrix*homogeneousCoordinates0
+      val clipCoordinates1 = modelViewProjectionMatrix*homogeneousCoordinates1
+
+      // For simplicity clipping is omited.
+
+      val normalizedDeviceCoordinates0 = clipCoordinates0.xyz/clipCoordinates0.w
+      val normalizedDeviceCoordinates1 = clipCoordinates1.xyz/clipCoordinates1.w
+
+      val windowCoordinates0 = normalizedDeviceCoordinates0.xy*viewportScale + viewportOffset
+      val windowCoordinates1 = normalizedDeviceCoordinates1.xy*viewportScale + viewportOffset
+      
+      linesOut(j) = windowCoordinates0
+      linesOut(j + 1) = windowCoordinates1
+      
+      i += 1
+    }
+
+    (linesOut, colors)
+  }
+}
+
+}
---
+++ Simplex3dConsole/src/example/simplex3d/lines/SimplexLogo.scala
@@ -0,0 +1,83 @@
+package example.simplex3d.lines
+
+import simplex3d.math._
+import simplex3d.math.double._
+import simplex3d.math.doublex.functions._
+import simplex3d.data._
+import simplex3d.data.double._
+import simplex3d.console.extension.ImageUtils._
+
+
+/**
+ * @author Aleksey Nikiforov (lex)
+ */
+object SimplexLogo extends App {
+
+{
+  // Simplex logo.
+  val side = 8.0
+  val height2d = sqrt(3.0/4.0*side*side)
+  val radius2d = side/sqrt(3.0)
+  val centerz = sqrt(radius2d*radius2d - 1.0/4.0*side*side)
+  val height3d = side*sqrt(6.0)/3.0
+  val radius3d = side*sqrt(3.0/8.0)
+  val centery = sqrt(radius3d*radius3d - radius2d*radius2d)
+
+  val simplexTranslation = Mat3x4 translate(-Vec3(side/2, centery, centerz))
+
+  val simplex = List(
+    // Base.
+    Vec3(0, 0, 0), Vec3(side, 0, 0),
+    Vec3(side, 0, 0), Vec3(side/2, 0, height2d),
+    Vec3(side/2, 0, height2d), Vec3(0, 0, 0),
+    // The remaining 3 edges.
+    Vec3(0, 0, 0), Vec3(side/2, height3d, centerz),
+    Vec3(side, 0, 0), Vec3(side/2, height3d, centerz),
+    Vec3(side/2, 0, height2d), Vec3(side/2, height3d, centerz)
+  ).map(p => simplexTranslation.transformPoint(p))
+
+
+  val lines = DataArray[Vec3, RFloat](simplex: _*)
+  val linesOut = DataArray[Vec2, RFloat](lines.size)
+  
+  val colors = DataArray[Vec3, UByte](lines.size)
+  for (i <- 0 until colors.size/2) { colors(i*2) = Vec3(1, 0, 0); colors(i*2 + 1) = Vec3(0, 0, 1) }
+
+
+  // All 3D transformations happen here.
+  def transform(modelMat: inMat3x4, cameraMat: inMat3x4, projectionMat: inMat4, dims: inVec2i) {
+    val modelViewProjectionMat = projectionMat*Mat4(modelMat.concat(inverse(cameraMat)))
+
+    val viewportScale = dims*0.5
+    val viewportOffset = dims*0.5
+
+    var i = 0; while (i < lines.size - 1) {
+      val c0 = modelViewProjectionMat*Vec4(lines(i), 1)
+      val c1 = modelViewProjectionMat*Vec4(lines(i + 1), 1)
+
+      val n0 = c0.xyz/c0.w
+      val n1 = c1.xyz/c1.w
+
+      linesOut(i) = n0.xy*viewportScale + viewportOffset
+      linesOut(i + 1) = n1.xy*viewportScale + viewportOffset
+
+      i += 2
+    }
+  }
+
+
+  animateLines("Simplex") { (dims, time) =>
+    val camRotation = Quat4 rotateX(radians(-20)) rotateY(radians(30)*time)
+    val camTranslation = camRotation.rotateVector(Vec3(0, 0, 20))
+
+    val cameraMat = Mat3x4 rotate(camRotation) translate(camTranslation)
+    val modelMat = Mat3x4 Identity
+    val projectionMat = perspectiveProj(radians(45), dims.x.toDouble/dims.y, 10, 100)
+
+    transform(modelMat, cameraMat, projectionMat, dims)
+
+    (linesOut, colors)
+  }
+}
+
+}

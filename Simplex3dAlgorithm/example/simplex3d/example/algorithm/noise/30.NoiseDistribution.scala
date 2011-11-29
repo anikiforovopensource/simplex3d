0b540455171412672a705b370b832fde68e44853
lexn82 <lexn82@fe83b338-bbbe-11de-ad49-1f49b0b801a7>
2011-11-29 06:48:26 +0000

    Added example sources to netbeans project. Further reorganized examples.

---
+++ Simplex3dAlgorithm/example/simplex3d/example/algorithm/noise/30.NoiseDistribution.scala
@@ -0,0 +1,43 @@
+package example.simplex3d.procedural.animation
+
+import simplex3d.math._
+import simplex3d.math.double._
+import simplex3d.math.double.functions._
+import simplex3d.algorithm.noise._
+import simplex3d.script.ImageUtils._
+
+
+/**
+ * @author Aleksey Nikiforov (lex)
+ */
+object NoiseDistribution extends App {
+
+  val zoom = 1.0/50
+  val changeSpeed1 = 1.0/3
+  val changeSpeed2 = 1.0/5
+  val scrollSpeed = 10
+
+  val noise = new Noise(ClassicalGradientNoise)
+
+  animateFunction("Noise Distribution") { (dims, time, pixel) =>
+    val p = pixel + time*scrollSpeed
+
+    val timeSlot = (time.toInt/10)%4
+    val n = {
+      if (timeSlot == 0) noise(p.x*zoom)
+      else if (timeSlot == 1) noise(p*zoom)
+      else if (timeSlot ==2) noise(Vec3(p*zoom, time*changeSpeed1))
+      else noise(Vec4(p*zoom, time*changeSpeed1, time*changeSpeed2))
+    }
+
+    val scaledNoise = (n + 1)/2
+    if (scaledNoise > 0.75) color(Vec3(1, 0, 0), (scaledNoise - 0.75)/0.25)
+    else if (scaledNoise > 0.5) color(Vec3(1, 0, 1), (scaledNoise - 0.5)/0.25)
+    else if (scaledNoise > 0.25) color(Vec3(0, 0, 1), (scaledNoise - 0.25)/0.25)
+    else color(Vec3(0, 1, 1), (scaledNoise)/0.25)
+  }
+
+  def color(shadeColor: inVec3, intencity: Double) :Vec3 = {
+    mix(shadeColor/2, shadeColor, intencity)
+  }
+}
---
+++ Simplex3dAlgorithm/example/simplex3d/example/algorithm/noise/31.NoiseDegradation.scala
@@ -0,0 +1,35 @@
+import simplex3d.math.double._
+import simplex3d.math.double.functions._
+import simplex3d.algorithm.noise._
+import simplex3d.script.ImageUtils._
+
+
+/**
+ * @author Aleksey Nikiforov (lex)
+ */
+object NoiseDegradation extends App {
+
+  
+  val scale = 1.0/50
+  val noiseSpeed = 1.0/3
+  val scrollSpeed = 10
+
+  val noise = new Noise(ClassicalGradientNoise)
+  
+  val f = 3.0
+  def largeOffset(time: Double) :Double = {
+    val local = time.toInt % 18
+    if (local < 3) 5e13
+    else if (local < 6) 5e13*f
+    else if (local < 9) 5e13*f*f
+    else if (local < 12) 5e13*f*f*f
+    else if (local < 15) 5e13*f*f*f*f
+    else 5e13*f*f*f*f*f
+  }
+
+  // Shows gradual noise degradation at large offsets.
+  animateFunction("Noise Degradation") { (dims, time, pixel) =>
+    val p = pixel + time*scrollSpeed
+    Vec3((noise(Vec3(p*scale + largeOffset(time), time*noiseSpeed)) + 1)/2)
+  }
+}

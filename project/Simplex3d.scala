a92d9fcc1d44028a32ce50e5f20dbf127d3d000a
lexn82 <lexn82@fe83b338-bbbe-11de-ad49-1f49b0b801a7>
2012-11-26 18:26:17 +0000

    Build: Added svn-ignore "project/project". Updated versions.

--- project/Simplex3d.scala
+++ project/Simplex3d.scala
@@ -26,14 +26,14 @@ object Simplex3d extends Build {
   val ScalaVersion = "2.9.2"
   val LwjglVersion = "2.8.4"
   
-  val MathVersion = "2.4.0"
-  val DataVersion = "2.4.0"
-  val AlgorithmNoiseVersion = "0.4.5"
-  val AlgorithmIntersectionVersion = "0.4.5"
-  val AlgorithmMeshVersion = "0.4.5"
-  val EngineVersion = "0.4.2"
-  val ConsoleVersion = "0.4.2-SNAPSHOT"
-  val ScriptVersion = "0.2.2-SNAPSHOT"
+  val MathVersion = "2.4.2-SNAPSHOT"
+  val DataVersion = "2.4.2-SNAPSHOT"
+  val AlgorithmNoiseVersion = "0.4.6-SNAPSHOT"
+  val AlgorithmIntersectionVersion = "0.4.6-SNAPSHOT"
+  val AlgorithmMeshVersion = "0.4.6-SNAPSHOT"
+  val EngineVersion = "0.3.3-SNAPSHOT"
+  val ConsoleVersion = "0.3.3-SNAPSHOT"
+  val ScriptVersion = "0.3.3-SNAPSHOT"
   
   val ScalatestVersion = "1.6.1"
   
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
 resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.7")
+//addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.7")
  
  
